f64c94731708768cd92a2153b2d096f0ddb8b1bd
blair909 <61510475+blair909@users.noreply.github.com>
2020-05-14 09:04:14 -0500

    First MVC Commit

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,5 +23,9 @@ namespace ElevenNote.Data
         public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset? ModifiedUtc { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set;}
     }
 }
